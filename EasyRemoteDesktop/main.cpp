b2121e06a67e77a31d384ea30ce4d9b7bafc88a7
xianglai <xianglai@cvte.com>
2019-11-09 17:25:55 +0800

    [Add] Finish basic ui for server point

---
+++ EasyRemoteDesktop/main.cpp
@@ -0,0 +1,10 @@
+﻿#include "EasyRemoteDesktop.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    EasyRemoteDesktop w;
+    w.show();
+    return a.exec();
+}
