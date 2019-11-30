34208dc51ec4ec4f758a8fb9314e15690a5d798f
xianglai <xianglai@cvte.com>
2019-11-30 12:33:55 +0800

    [Add] 添加glog和截屏库

--- EasyRemoteDesktop/src/main.cpp
+++ EasyRemoteDesktop/src/main.cpp
@@ -1,10 +1,16 @@
 ﻿#include "EasyRemoteDesktop.h"
 #include <QtWidgets/QApplication>
 
+#include "glog/logging.h"
+#pragma comment(lib, "glog.lib")
+
 int main(int argc, char *argv[])
 {
+    google::InitGoogleLogging(argv[0]);
+    google::SetLogDestination(google::GLOG_INFO, "log_");
     QApplication a(argc, argv);
     EasyRemoteDesktop w;
     w.show();
+    LOG(INFO) << "Start running...";
     return a.exec();
 }
