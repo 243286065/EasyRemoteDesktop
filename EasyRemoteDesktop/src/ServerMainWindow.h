31c9d40f3df5dce17da5ee9b46293e10b6cbb04f
xianglai <xianglai@cvte.com>
2019-12-07 11:51:37 +0800

    [Add] 完成本地预览功能

--- EasyRemoteDesktop/src/ServerMainWindow.h
+++ EasyRemoteDesktop/src/ServerMainWindow.h
@@ -2,24 +2,31 @@
 #include <qobject.h>
 
 #include "ui_EasyRemoteDesktop.h"
+#include "CaptureScreenThread.h"
 
 class ServerMainWindow :
     public QObject
 {
     Q_OBJECT
+public slots:
+    void onGetNewFrame();
 
 private slots:
     void onPreviewBtnClicked();
     void onSentBtnClicked();
+    void onFpsSelectedChanged(const QString& fps);
 
 public:
     ServerMainWindow(Ui::EasyRemoteDesktopClass *ui);
     ~ServerMainWindow();
 
 private:
+    void CaptureStatusChange();
     Ui::EasyRemoteDesktopClass *m_pUi;
 
     bool m_bPreviewFlag = false;
     bool m_bSentFlag = false;
+
+    CaptureScreenThread m_captureThread;
 };
 
--- EasyRemoteDesktop/src/main.cpp
+++ EasyRemoteDesktop/src/main.cpp
@@ -3,11 +3,13 @@
 
 #include "glog/logging.h"
 #pragma comment(lib, "glog.lib")
+#pragma comment(lib, "gflags_nothreads_static.lib")
 
 int main(int argc, char *argv[])
 {
     google::InitGoogleLogging(argv[0]);
     google::SetLogDestination(google::GLOG_INFO, "log_");
+    //FLAGS_alsologtostderr = true;
     QApplication a(argc, argv);
     EasyRemoteDesktop w;
     w.show();
